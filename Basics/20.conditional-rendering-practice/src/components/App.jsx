25a42410089b65f16ff1d383be3e68c5a853810d
Prince Bansal <prince.bansal008@gmail.com>
2022-05-28 04:41:55 +0530

    practice added

---
+++ Basics/20.conditional-rendering-practice/src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Form from "./Form";
+
+// var userIsRegistered = true;
+var userIsRegistered = false;
+
+function App() {
+  return (
+    <div className="container">
+      <Form isRegistered={userIsRegistered} />
+    </div>
+  );
+}
+
+export default App;
