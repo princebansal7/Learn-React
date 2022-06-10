cce5c795d02bbba07f2c411c35ab77c83b7830b7
Prince Bansal <prince.bansal008@gmail.com>
2022-06-10 13:25:55 +0530

    practice added

---
+++ Basics/29.changing-complex-state-practice/src/components/App.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+
+function App() {
+  const [contact, setContact] = useState({
+    fName: "",
+    lName: "",
+    email: "",
+  });
+
+  function handleChange(event) {
+    // WAY-1
+    // const inputName = event.target.name;
+    // const newValue = event.target.value;
+
+    // WAY-2
+    const { name, value } = event.target;
+
+    setContact((newObj) => {
+      if (name === "fName") {
+        return {
+          fName: value,
+          lName: newObj.lName,
+          email: newObj.email,
+        };
+      } else if (name === "lName") {
+        return {
+          fName: newObj.fName,
+          lName: value,
+          email: newObj.email,
+        };
+      } else if (name === "email") {
+        return {
+          fName: newObj.fName,
+          lName: newObj.lName,
+          email: value,
+        };
+      }
+    });
+  }
+
+  return (
+    <div className="container">
+      <h1>
+        Hello {contact.fName} {contact.lName}
+      </h1>
+      <p>{contact.email}</p>
+      <form>
+        <input
+          name="fName"
+          placeholder="First Name"
+          value={contact.fName}
+          onChange={handleChange}
+        />
+        <input
+          name="lName"
+          placeholder="Last Name"
+          value={contact.lName}
+          onChange={handleChange}
+        />
+        <input
+          name="email"
+          placeholder="Email"
+          value={contact.email}
+          onChange={handleChange}
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
